55a4488ae608ce82e6eaf75ac6d03deb5129c52f
Chisom Daniel <chinwezechisom@gmail.com>
2023-09-29 00:21:17 +0100

    task3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+    constructor(code, name) {
+        this._code = code,
+        this._name = name
+    }
+
+    set code(x) {
+        this._code = x;
+    }
+
+    set name(y) {
+        this._name = y;
+    }
+
+    get code() {
+        return this._code;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`
+    }
+}
