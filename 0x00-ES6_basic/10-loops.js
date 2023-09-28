650a8b04fa48127276426c679346f67e36c501b3
Chisom Daniel <chinwezechisom@gmail.com>
2023-09-28 10:36:12 +0100

    task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
   let i = 0;
-  const newList = [...array];
-  for (const item of newList) {
-    newList[i] = appendString + item;
+  for (const item of array) {
+    array[i] = appendString + item;
     i += 1;
   }
 
-  return newList;
+  return array;
 }
