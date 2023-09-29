e97348a5a22eb0a04ed9649292549161c875c6d4
Chisom Daniel <chinwezechisom@gmail.com>
2023-09-29 10:35:17 +0100

    style fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,8 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (!(this.constructor == Building)) {
-        this.evacuationWarningMessage();
+    if (!(this.constructor === Building)) {
+      this.evacuationWarningMessage();
     }
   }
 
