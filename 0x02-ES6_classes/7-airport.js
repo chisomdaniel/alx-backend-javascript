d203945242d50260886ed6d73b349790aa2105d5
Chisom Daniel <chinwezechisom@gmail.com>
2023-09-30 00:25:03 +0100

    task 7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,10 +1,29 @@
 export default class Airport {
-    constructor(name, code) {
-        this._name = name;
-        this._code = code;
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-    toString() {
-        return this._code;
-    }
+  toString() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(newName) {
+    this._name = newName;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Setter for code
+  set code(newCode) {
+    this._code = newCode;
+  }
 }
