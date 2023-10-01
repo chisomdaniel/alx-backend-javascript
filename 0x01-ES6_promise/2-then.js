e1ad160cb9a55ce14585bd071f1d67c7a72c6991
Chisom Daniel <chinwezechisom@gmail.com>
2023-10-01 21:22:21 +0100

    first 3 task

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+function handleResponseFromAPI(promise) {
+    promise.then(()=> {
+        return {status: 200, body: success};
+    }).catch(()=> {
+        return new Error()
+    }).finally(
+        console.log('Got a response from the API')
+    )
+}
