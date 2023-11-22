3b8e7cee487471470947b897a2b7b4cd67ee5dbc
Chisom Daniel <chinwezechisom@gmail.com>
2023-11-22 15:02:28 +0100

    mistake

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    let data = fs.readFileSync(path, 'utf8');
+    data = data.split('\n');
+
+    const csList = [];
+    const sweList = [];
+    let studentCount = 0;
+    let countCS = 0;
+    let countSWE = 0;
+    data.forEach((value) => {
+      const dataTray = value.split(',');
+      if (dataTray[dataTray.length - 1] === 'CS') {
+        studentCount += 1;
+        countCS += 1;
+        csList.push(dataTray[0]);
+      } else if (dataTray[dataTray.length - 1] === 'SWE') {
+        studentCount += 1;
+        countSWE += 1;
+        sweList.push(dataTray[0]);
+      }
+    });
+    console.log(`Number of students: ${studentCount}`);
+    console.log(`Number of students in CS: ${countCS}. List: ${csList.join(', ')}`);
+    console.log(`Number of students in SWE: ${countSWE}. List: ${sweList.join(', ')}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
